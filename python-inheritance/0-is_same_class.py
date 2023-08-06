0b6aa63f36e6553cb67ea0be200796726185986d
Pearlhummy <igbinladepelumi@gmail.com>
2023-08-06 20:14:59 +0100

    same class task

--- python-inheritance/0-is_same_class.py
+++ python-inheritance/0-is_same_class.py
@@ -1,7 +1,12 @@
+"""
+fonction that check instance of a class
 
+"""
 
 def is_same_class(obj, a_class):
-   
+    """
+    the fonction
+    """
     if type(obj)==a_class:
         return True
     else:
