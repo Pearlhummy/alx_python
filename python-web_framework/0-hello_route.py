95df02aebf31cbf0906fbadc935eb5fea2a1b804
Pearlhummy <igbinladepelumi@gmail.com>
2023-09-21 00:37:24 +0100

    all task for web framework done

---
+++ python-web_framework/0-hello_route.py
@@ -0,0 +1,14 @@
+"""Script that starts a Flask web application & displays a value(str)"""
+
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    return 'Hello HBNB!'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
